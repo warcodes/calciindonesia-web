19a8584929bc84d124d342df23dac558abc3a647
zul1996 <mzulnewbie@gmail.com>
2022-09-08 19:13:59 +0700

    add arrival section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', () => {
   const nav = document.getElementsByTagName('nav')[0];
-  if(window.scrollY > 30 ){
+  if (window.scrollY > 30) {
     nav.classList.add('bg-black');
   } else {
     nav.classList.remove('bg-black');
