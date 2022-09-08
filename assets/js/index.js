2a9dd8010788b67d8361b17ef9495a1667f39413
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-08 16:18:15 +0700

    feat: add function navbar change color to black when scroll

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  const nav = document.getElementsByTagName('nav')[0];
+  if(window.scrollY > 30 ){
+    nav.classList.add('bg-black');
+  } else {
+    nav.classList.remove('bg-black');
+  }
+});
