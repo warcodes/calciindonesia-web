c54a159bcf96742340022d61cc5e2cecc42078cc
Fikri Alwan Ramadhan <fikriar93@gmail.com>
2022-09-10 17:12:32 +0700

    feat: add function show navbar list when mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,29 @@
-window.addEventListener('scroll', () => {
-  const nav = document.getElementsByTagName('nav')[0];
-  if (window.scrollY > 30) {
-    nav.classList.add('bg-black');
-  } else {
-    nav.classList.remove('bg-black');
-  }
+const isHome = document.getElementsByClassName('active')[0].textContent === 'Home';
+
+if (isHome) {
+  window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav-transparent');
+    if (window.scrollY > 30) {
+      nav.classList.add('bg-black');
+    } else {
+      nav.classList.remove('bg-black');
+    }
+  });
+}
+
+
+const menuButton = document.getElementsByClassName('menu')[0];
+const closeButton = document.getElementsByClassName('close')[0];
+const navMobile = document.getElementsByClassName('nav-mobile')[0];
+
+menuButton.addEventListener('click', () => {
+  navMobile.classList.add('show');
+  closeButton.classList.add('show');
+  menuButton.classList.add('hidden');
 });
+
+closeButton.addEventListener('click', () => {
+  navMobile.classList.remove('show');
+  closeButton.classList.remove('show');
+  menuButton.classList.remove('hidden');
+})
